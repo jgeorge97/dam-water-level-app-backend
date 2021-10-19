caa46d1ba5b355f96f5231fe5cc107f56ae64411
George Jose <jgeorge97@gmail.com>
2021-10-19 16:31:58 +0530

    Added CORS for all routes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const app = require('./app');
+
 require('dotenv').config()
+
+var cors = require('cors')
+app.use(cors())
 
 const port = process.env.PORT || 3000;
 
